8e18c5c987e61d46ebf0adb8b47b1ff22e6ea45a
RainCreatorHub <rain.hub.creator@gmail.com>
2025-10-05 02:44:05 -0300

    Create Icons.lua

---
+++ src/Utils/Icons.lua
@@ -0,0 +1,13 @@
+-- Icons.lua
+-- Utilitário para armazenar ícones em formato AssetId
+
+local Icons = {
+    Info = "rbxassetid://6035047377",
+    Warning = "rbxassetid://6031071050",
+    Error = "rbxassetid://6031071063",
+    Success = "rbxassetid://6031094678",
+    Close = "rbxassetid://6031094678",
+    Tab = "rbxassetid://6031154872"
+}
+
+return Icons
