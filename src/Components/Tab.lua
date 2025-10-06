fb3d4b56db71b570c9880060485ded8cb6e37311
RainCreatorHub <rain.hub.creator@gmail.com>
2025-10-06 02:44:13 -0300

    Update Tab.lua

--- src/Components/Tab.lua
+++ src/Components/Tab.lua
@@ -268,4 +268,11 @@ function Tab:Button(buttonConfig)
     return newButton
 end
 
+function Section:Dropdown(dropdownConfig)
+    local DropdownModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/RainCreatorHub/Deep-Lib/refs/heads/main/src/Elements/Dropdown.lua"))()
+    local newDropdown = DropdownModule.new(dropdownConfig, self.ContentFrame)
+    table.insert(self.Elements, newDropdown)
+    return newDropdown
+end
+
 return Tab
